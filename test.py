389c09018dd6fbd4dac12703c226d6b87fa16e82
Eric <hier0002@h-ka.de>
2022-01-05 14:12:54 +0100

    FirstCommit

---
+++ test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+filename = 'C:\\Hochschule\\WS21_22\\KI1\\Programme\\Vorles_tensorflow\\Aufgabe\\test.png'
+img = cv.imread(filename)
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+gray = np.float32(gray)
+dst = cv.cornerHarris(gray,2,3,0.04)
+#result is dilated for marking the corners, not important
+dst = cv.dilate(dst,None)
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+cv.imshow('dst',img)
+if cv.waitKey(0) & 0xff == 27:
+    cv.destroyAllWindows()
+    print("Hgfdsdfsfgfidunkey)")

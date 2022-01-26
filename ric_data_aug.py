23e6f3a985d92913b416102f89fd31d1617cece4
Eric <hier0002@h-ka.de>
2022-01-26 19:35:34 +0100

    finished data_aug

--- ric_data_aug.py
+++ ric_data_aug.py
@@ -17,16 +17,16 @@ class data_aug(ritt.imgorga):
 
     def setimg(self,img_path):
         img = cv2.imread(img_path)
-        resize = cv2.resize(img, (50, 50))
+        resize = cv2.resize(img, (100, 100))
         self.img = copy.deepcopy(resize)
         self.save_img = copy.deepcopy(resize)
         return 1
 
     def gray(self):
-        self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+        self.save_img = cv2.cvtColor(self.save_img, cv2.COLOR_BGR2GRAY)
         
     def blur(self):
-        self.save_img = cv2.GaussianBlur(self.save_img, (7, 7), 0)
+        self.save_img = cv2.GaussianBlur(self.save_img, (5, 5), 0)
         return 1
 
     def rotate_img(self):
@@ -38,43 +38,43 @@ class data_aug(ritt.imgorga):
 
     def flip_img(self):
         VH = np.random.randint(0,1)
-        self.img = cv2.flip(self.img, VH)
+        self.save_img = cv2.flip(self.save_img, VH)
 
     def add_noise(self, noise_type="gauss"):
         if noise_type == "gauss":
             mean=0
-            st=0.7
-            gauss = np.random.normal(mean,st,self.save_image.shape)
+            st=0.5
+            gauss = np.random.normal(mean,st,self.save_img.shape)
             gauss = gauss.astype('uint8')
-            image = cv2.add(self.save_image,gauss)
+            self.save_img = cv2.add(self.save_img,gauss)
         
         elif noise_type == "sp":
             prob = 0.05
-            if len(self.save_image.shape) == 2:
+            if len(self.save_img.shape) == 2:
                 black = 0
                 white = 255            
             else:
-                colorspace = self.save_image.shape[2]
+                colorspace = self.save_img.shape[2]
                 if colorspace == 3:  # RGB
                     black = np.array([0, 0, 0], dtype='uint8')
                     white = np.array([255, 255, 255], dtype='uint8')
                 else:  # RGBA
                     black = np.array([0, 0, 0, 255], dtype='uint8')
                     white = np.array([255, 255, 255, 255], dtype='uint8')
-            probs = np.random.random(self.save_image.shape[:2])
-            self.save_image[probs < (prob / 2)] = black
-            self.save_image[probs > 1 - (prob / 2)] = white
+            probs = np.random.random(self.save_img.shape[:2])
+            self.save_img[probs < (prob / 2)] = black
+            self.save_img[probs > 1 - (prob / 2)] = white
 
     def do_random_aug(self):
         choice = np.random.randint(2, size=5)
         if choice[0]:
-            self.add_noise()
-            self.name += "N"
-        else:
-            self.name += "0"
-        if choice[1]:
             self.blur()
             self.name += "B"
+        else:
+            self.add_noise()
+            self.name += "N"
+        if choice[1]:
+            self.name += "U"
         else:
             self.name += "0"
         if choice[2]:
@@ -83,7 +83,7 @@ class data_aug(ritt.imgorga):
         else:
             self.name += "0"
         if choice[3]:
-            self.rotat_img()
+            self.rotate_img()
             self.name += "R"
         else:
             self.name += "0"
@@ -94,19 +94,21 @@ class data_aug(ritt.imgorga):
             self.name += "0"
 
     def save_imgs(self,new_location):
+        os.makedirs(new_location, exist_ok=True)
         for idx,label in enumerate(self.labels):
             path_with_label = os.path.join(new_location,label)
-            os.mkdir(path_with_label)
+            os.makedirs(path_with_label, exist_ok=True)
             for img_path in self.image_paths[idx]:
                 filename = os.path.basename(img_path)
                 filetype = os.path.splitext(img_path)[1]
                 self.setimg(img_path)
-                self.do_random_aug(self)
+                self.do_random_aug()
                 save1 = os.path.join(path_with_label, filename + filetype)
                 str
                 save2 = os.path.join(path_with_label, filename+ self.name + filetype)
                 cv2.imwrite(save1,self.img)
-                cv2.imwrite(save2,self.img)
+                cv2.imwrite(save2,self.save_img)
+                self.name = "_"
         return 1
 
 

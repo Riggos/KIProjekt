fa5804fcde513ec65646d81d7a1986070cbeed56
Jonas Morsch <jonas.morsch@t-online.de>
2022-01-27 18:46:19 +0100

    CNN fertig

--- Data_vorbereitung.py
+++ Data_vorbereitung.py
@@ -1,3 +1,4 @@
+from tkinter import Image
 import pandas as pd
 import seaborn as sns
 import random
@@ -5,6 +6,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+import cv2
 
 
 class Data_preparation:
@@ -84,3 +87,78 @@ class Data_preparation:
                    size=6, aspect=1.5)  # Plot a scatterplot for two chosen columns
         # draw the potential Splits for the chosen row
         plt.vlines(x=potential_splits[feature_to_split], ymin=7, ymax=80)
+
+    def load_pictures(self, folder_location):
+        path = 'animal/'
+        img_dict = dict()
+
+        for root, dirs, files in os.walk(path):
+            print(os.path.basename(root))
+            my_key = os.path.basename(root)
+
+            dir_images = []
+            for file_ in files:
+                full_file_path = os.path.join(root, file_)
+                img = cv2.imread(full_file_path)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                dir_images.append(img)
+
+            img_dict[my_key] = dir_images
+
+        return img_dict
+
+    def create_dataset(self, img_folder, IMG_HEIGHT, IMG_WIDTH):
+
+        img_data_array = []
+        class_name = []
+
+        for dir1 in os.listdir(img_folder):
+            for file in os.listdir(os.path.join(img_folder, dir1)):
+
+                image_path = os.path.join(img_folder, dir1,  file)
+                image = cv2.imread(image_path)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                image = cv2.resize(
+                    image, (IMG_HEIGHT, IMG_WIDTH), interpolation=cv2.INTER_AREA)
+                image = np.array(image)
+                image = image.astype('float32')
+                image /= 255
+                img_data_array.append(image)
+                class_name.append(dir1)
+        return img_data_array, class_name
+
+    def split_Image_data(self, Images, Labels, train_size):
+
+        index_list = []  # an empty list for the indicies
+        test_index = []
+        X_test = []
+        X_train = []
+        y_test = []
+        y_train = []
+
+        if isinstance(train_size, float):
+            test_size = round(train_size * len(Images))
+
+        for i in range(len(Images)):
+            index_list.append(i)
+
+        # get random indizes for the split
+        train_indices = random.sample(population=index_list, k=test_size)
+        for i in range(len(Images)):
+            if i not in train_indices:
+                test_index.append(i)
+
+        for index in train_indices:
+            X_train.append(Images[index])
+            y_train.append(Labels[index])
+
+        for index in test_index:
+            X_test.append(Images[index])
+            y_test.append(Labels[index])
+
+        X_train = np.asarray(X_train)
+        y_train = np.array(y_train)
+        X_test = np.array(X_test)
+        y_test = np.array(y_test)
+
+        return X_train, X_test, y_train, y_test
